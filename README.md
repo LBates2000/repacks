7f74b0c110c0fa4f14a58bf07b0c900ed64bf605
LBates2000 <Lawrence.Bates@system1.com>
2022-08-19 03:36:57 -0700

    Commit script and README.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # repacks
-Find cbr and cbz repacks
+A simple bash script to find cbr and cbz repacks in a library and move them to a new directory. Requires.7-Zip.
---
+++ find-repacks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+library="/d/Comics"
+repackFile="ComicInfo.xml"
+repackDir="/d/ComicsRepacks"
+mkdir -p "$repackDir"
+for comic in "$library"/*.cb? "$library"/**/*.cb? ; do
+  repack=$(7z l "$comic" "$repackFile" -ba) # undocumented 7zip switch
+  if [ "$repack" ]
+    then
+      echo "$comic"
+      mv "$comic" "$repackDir/"
+  fi
+done
