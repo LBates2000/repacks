daded5f9560660794484db3d39d7f3036c65d322
Lawrence Bates <3049531+LBates2000@users.noreply.github.com>
2022-08-20 01:51:57 -0700

    Update find-repacks.sh

--- find-repacks.sh
+++ find-repacks.sh
@@ -5,7 +5,7 @@ repackFile="ComicInfo.xml"
 repackDir="/d/ComicsRepacks"
 mkdir -p "$repackDir"
 for comic in "$library"/*.cb? "$library"/**/*.cb? ; do
-  repack=$(7z l "$comic" "$repackFile" -ba) # undocumented 7zip switch
+  repack=$(7z l "$comic" "$repackFile" -ba) # undocumented 7-zip switch
   if [ "$repack" ]
     then
       echo "$comic"
